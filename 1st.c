40d190e06aef87a067871538c71cecff3cb4aef6
Saarth <saadhuishan@gmail.com>
2026-02-13 10:52:28 +0100

    Add files via upload
    Sum of two numbers.

---
+++ 1st.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int e,f, sum;
+    printf("My section is G2M\n");
+    printf("Enter the value of e and f :");
+    scanf("%d;%d", &e,&f);
+        sum=e+f;
+        printf("The sum of e and f is - %d", sum);
+    return(0);
+}
